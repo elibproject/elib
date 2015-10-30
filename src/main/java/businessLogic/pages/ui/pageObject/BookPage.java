a44debc637b05cb39c87af28df4462f44d445fef
Denvergoa <walder@ukr.net>
2015-10-30 10:54:40 +0200

    2.1 add FilterPage

--- src/main/java/businessLogic/pages/ui/pageObject/BookPage.java
+++ src/main/java/businessLogic/pages/ui/pageObject/BookPage.java
@@ -2,8 +2,5 @@ package businessLogic.pages.ui.pageObject;
 
 import net.serenitybdd.core.pages.PageObject;
 
-/**
- * Created by Irinka on 10/19/2015.
- */
 public class BookPage extends PageObject {
 }
