1baf2d6fdea9c33eef676eb9e98ba8e4dfeb2e0e
IrynaKoryttseva <Iryna_Koryttseva@epam.com>
2015-11-05 13:36:49 +0200

    1.17(action with file)

--- src/main/java/addContentForTesting/ActionWithFile.java
+++ src/main/java/addContentForTesting/ActionWithFile.java
@@ -1,32 +1,63 @@
 package addContentForTesting;
 
 
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import org.apache.poi.ss.extractor.ExcelExtractor;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 public class ActionWithFile {
 
-    public static void main(String[] args) throws Exception {
-        String comments = "C:/Users/Iryna_Koryttseva/IdeaProjects/elib/src/main/resources/listComments.xls";
-        String authors = "C:/Users/Iryna_Koryttseva/IdeaProjects/elib/src/main/resources/listAuthors.xls";
-        System.out.println("You can choose the one word, what the test content need to add, input word: comments, authors,publishers, books");
+    public static void main(String[] args) throws BiffException, IOException {
+
+        ActionWithFile actionWithFile = new ActionWithFile();
+        actionWithFile.readExcelFile();
 
     }
 
-   /* public static void main(String[] args) throws Exception {
-        InputStream contentTest = new FileInputStream("C:/Users/Iryna_Koryttseva/IdeaProjects/elib/src/main/resources/listComments.xls");
-        HSSFWorkbook wb = new HSSFWorkbook(contentTest);
-       // ExcelExtractor extractor = new ExcelExtractor(wb);
 
+    public void readExcelFile() throws BiffException, IOException {
+        String comments = "C:\\Users\\Iryna_Koryttseva\\IdeaProjects\\elib\\src\\main\\resources\\listTestText.xls";
+        FileInputStream text = new FileInputStream(comments);
+        Workbook wb = Workbook.getWorkbook(text);
+        // TO get the access to the sheet
+        Sheet sh = wb.getSheet("comments");
+        //Sheet sh = wb.getSheet("authors");
+        // Sheet sh = wb.getSheet("publishers");
+        // Sheet sh = wb.getSheet("books");
+        // To get the number of rows present in sheet
+        int totalNoOfRows = sh.getRows();
+        // To get the number of columns present in sheet
+        int totalNoOfCols = sh.getColumns();
+        for (int row = 0; row < totalNoOfRows; row++) {
+            for (int col = 0; col < totalNoOfCols; col++) {
+                System.out.print(sh.getCell(col, row).getContents() + "\t");
+            }
+            System.out.println();
+        }
+        ExcelExtractor extractor = new ExcelExtractor(wb)
         extractor.setFormulasNotResults(false); // Считать формулы
         extractor.setIncludeSheetNames(true);
-        String text = extractor.getText();
-
+        String[] text = extractor.getText();
         System.out.println(text);
+        {
+            // @Override
+            //  public String getText() {
+            //      return null;
+            //    }
+
+        }
+
+
 
     }
-}
-public class ActionWithFile {
+
+/*public class ActionWithFile {
 
     public static void readExcelFile(String File)throws IOException {
-        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream("C:/Users/Iryna_Koryttseva/IdeaProjects/elib/src/main/resources/listComments.xls"));
+        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream("C:/Users/Iryna_Koryttseva/IdeaProjects/elib/src/main/resources/listTestText.xls"));
         HSSFSheet myExcelSheet = myExcelBook.getSheet("Birthdays");
         HSSFRow row = myExcelSheet.getRow(0);
 
@@ -42,7 +73,7 @@ public class ActionWithFile {
 
         myExcelBook.close();
     }*/
-}
+//}*/
 
 
 
